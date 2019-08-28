2b86a9bb65562565b57cc6ead50136b84837ce06
Dominique Skaf <doskaf@yahoo.com>
2019-08-28 19:48:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,7 @@ def hit?(number)
   user_input = get_user_input
   if user_input == 'h'
     card_total += deal_card
-    if get_user_input = 's'
+    elsif user_input == 's'
       card_total = card_total
     else
       invalid_command
